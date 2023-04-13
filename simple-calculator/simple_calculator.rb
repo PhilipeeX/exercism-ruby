d3585d0234d6c7e531ad0be081857caa3e27f0b7
PhilipeeX <dev.philipe@outlook.com>
2023-04-13 13:24:34 -0300

    Tentativa falha de resolver o exercício simple-calculator, nenhum erro resolvido

--- simple-calculator/simple_calculator.rb
+++ simple-calculator/simple_calculator.rb
@@ -1,7 +1,19 @@
+require 'pry-byebug'
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
   
-  def self.calculate(first_operand, second_operand, operation)
-   
+  begin
+    def self.calculate(first_operand, second_operand, operation)
+          binding.pry
+      if operation == "/" && second_operand == 0
+        return "Division by zero is not allowed."
+      elsif first_operand.class != Integer || second_operand.class != Integer
+        raise ArgumentError, "O valores de entrada devem ser números"
+      elsif 
+        "#{first_operand} #{operation} #{second_operand} = #{first_operand.send(operation, second_operand)}"
+      end
+    end
+  rescue => e
+    raise UnsupportedOperation
   end
 end
