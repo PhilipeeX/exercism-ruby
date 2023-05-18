666766a6aa59b6f79eb36f20f526de87696d8277
PhilipeeX <dev.philipe@outlook.com>
2023-05-18 16:32:41 -0300

    Exercício roman-numerals iniciado, metodo to_roman criado e teste numero 1 resolvido

---
+++ roman-numerals/roman_numerals.rb
@@ -0,0 +1,37 @@
+=begin
+Write your code for the 'Roman Numerals' exercise in this file. Make the tests in
+`roman_numerals_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/roman-numerals` directory.
+=end
+
+class Integer
+  def to_roman
+    # require 'pry-byebug'; binding.pry
+    numero = self
+    romanos = {
+      1000 => 'M',
+      900 => 'CM',
+      500 => 'D',
+      400 => 'CD',
+      100 => 'C',
+      90 => 'XC',
+      50 => 'L',
+      40 => 'XL',
+      10 => 'X',
+      9 => 'IX',
+      5 => 'V',
+      4 => 'IV',
+      1 => 'I'
+    }
+    resultado = ''
+    romanos.each do |valor, romano|
+      while numero >= valor
+        resultado << romano
+        numero -= valor
+      end
+    end
+    resultado
+  end
+end
