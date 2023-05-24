9eee9c6029680c5cc7a10ed4b848baf4be6c3111
PhilipeeX <dev.philipe@outlook.com>
2023-05-24 19:09:24 -0300

    testes: test_addition_with, test_multiplication e test_division implementados sem erros

--- simple-calculator/simple_calculator.rb
+++ simple-calculator/simple_calculator.rb
@@ -1,19 +1,18 @@
-require 'pry-byebug'
+
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
-  
-  begin
-    def self.calculate(first_operand, second_operand, operation)
-          binding.pry
-      if operation == "/" && second_operand == 0
-        return "Division by zero is not allowed."
-      elsif first_operand.class != Integer || second_operand.class != Integer
-        raise ArgumentError, "O valores de entrada devem ser números"
-      elsif 
+
+  def self.calculate(first_operand, second_operand, operation)
+    if second_operand == 0
+      raise "Division by zero is not allowed."
+    end
+    begin
+      # require 'pry-byebug'; binding.pry
+      if ALLOWED_OPERATIONS.any? {|operacao| operacao == operation}
         "#{first_operand} #{operation} #{second_operand} = #{first_operand.send(operation, second_operand)}"
       end
+    rescue UnsupportedOperation => e
+      puts "Operação não suportada: #{e.message}"
     end
-  rescue => e
-    raise UnsupportedOperation
   end
 end
